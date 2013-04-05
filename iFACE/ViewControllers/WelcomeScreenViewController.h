2bca2d8fe187a9b6b030da8e3c3fec66dc28fb41
jylups <jylups@gmail.com>
2013-04-05 18:32:28 -0400

    framework functionality

---
+++ iFACE/ViewControllers/WelcomeScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeScreenViewController.h
+//  iFACE
+//
+//  Created by Jorge Carvallo on 4/5/13.
+//  Copyright (c) 2013 Deloitte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeScreenViewController : UIViewController
+
+@end
---
+++ iFACE/ViewControllers/WelcomeScreenViewController.m
@@ -0,0 +1,27 @@
+//
+//  WelcomeScreenViewController.m
+//  iFACE
+//
+//  Created by Jorge Carvallo on 4/5/13.
+//  Copyright (c) 2013 Deloitte. All rights reserved.
+//
+
+#import "WelcomeScreenViewController.h"
+
+@implementation WelcomeScreenViewController
+
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
