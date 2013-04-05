04bfbad2f84884e150e3e8c3fd9893ac80a2843e
jylups <jylups@gmail.com>
2013-04-05 00:20:11 -0400

    initial commit

---
+++ iFACE/iFACE/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  iFACE
+//
+//  Created by Jorge Carvallo on 4/5/13.
+//  Copyright (c) 2013 Deloitte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ iFACE/iFACE/DetailViewController.m
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.m
+//  iFACE
+//
+//  Created by Jorge Carvallo on 4/5/13.
+//  Copyright (c) 2013 Deloitte. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+- (void)configureView;
+@end
+
+@implementation DetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem
+{
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+        
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView
+{
+    // Update the user interface for the detail item.
+
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [[self.detailItem valueForKey:@"timeStamp"] description];
+    }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
