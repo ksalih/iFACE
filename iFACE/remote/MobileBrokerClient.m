cbbea1f16449db9325ecb9dc4a8934f6294e9bf8
Karwan Salih <ksalih@deloitte.com>
2013-04-06 17:44:43 -0400

    Whatever

--- iFACE/remote/MobileBrokerClient.m
+++ iFACE/remote/MobileBrokerClient.m
@@ -164,7 +164,7 @@
         if ([qr.records count]>0){
             zkSObject = [qr.records objectAtIndex:0];
             DPerson *person = [IFACECoredataHelper addOrUpdatePerson:userInfo withManagedObjectContext:self.managedObjectContext];
-            //copy data from zkSObject into person and do a managedObjectContext save
+            //copy data from zkSObject into person and do a managedObjectContext save Karwan
             
             if (person){
                 if ([self.delegate respondsToSelector:@selector(mobileBrokerClient:didFinishSynchronizingUser:)]){
