b966b63c6c4e38a2f63c4ada081837f62230e406
jylups <jylups@gmail.com>
2013-04-07 05:42:38 -0400

    activity

--- iFACE/ViewControllers/AllActivityViewController.m
+++ iFACE/ViewControllers/AllActivityViewController.m
@@ -25,7 +25,7 @@
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-   // [[MobileBrokerClient sharedClient] syncActivityInformation];
+    [[MobileBrokerClient sharedClient] syncActivityInformation];
 }
 
 - (void)didReceiveMemoryWarning
